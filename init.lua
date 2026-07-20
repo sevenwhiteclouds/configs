93171129edb90863425cede26c2b9b241f28ab7e
ReplicantK <maldonadokeldin@gmail.com>
2026-07-20 16:54:50 -0700

    vim.pack: redefine how plugins are installed

--- init.lua
+++ init.lua
@@ -8,15 +8,34 @@ if not (profile == "stock") then
   require("cmds")
 end
 
-if profile == "minimal" or profile == "full" then
-  require("pack")
-  --require("lazyconf")
-  require("telescopeconf")
-end
+-- plenary is a telescope.nvim dependency
+if profile == "minimal" then
+  vim.pack.add({
+    "https://github.com/nvim-lua/plenary.nvim",
+    {src = "https://github.com/nvim-telescope/telescope.nvim", vim.version.range("v0.2.1")}
+  })
 
-if profile == "full" then
+  require("telescopeconf")
+elseif profile == "full" then
+  vim.pack.add({
+    "https://github.com/nvim-lua/plenary.nvim",
+    {src = "https://github.com/nvim-telescope/telescope.nvim", vim.version.range("v0.2.1")},
+
+    "https://github.com/williamboman/mason.nvim",
+    "https://github.com/williamboman/mason-lspconfig.nvim",
+
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/hrsh7th/cmp-nvim-lsp",
+
+    "https://github.com/hrsh7th/nvim-cmp",
+    {src = "https://github.com/L3MON4D3/LuaSnip", vim.version.range("v2.x")},
+
+    {src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master"}
+  })
+
+  require("telescopeconf")
   require("masonconf")
   require("lspconfig")
-  require("treesitconf")
   require("autocmp")
+  require("treesitconf")
 end
